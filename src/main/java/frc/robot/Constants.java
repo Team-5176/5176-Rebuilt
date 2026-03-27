b6bf03d4bfb9105bd2aeeb5b26a61ba52744a6ad
SLUH Robotics SSH Account <robotics@sluh.org>
2026-03-27 11:43:46 -0500

    increase speeds

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -27,7 +27,7 @@ public final class Constants
   public static final double ROBOT_MASS = (148 - 20.3) * 0.453592; // 32lbs * kg per pound
   public static final Matter CHASSIS    = new Matter(new Translation3d(0, 0, Units.inchesToMeters(8)), ROBOT_MASS);
   public static final double LOOP_TIME  = 0.13; //s, 20ms + 110ms sprk max velocity lag
-  public static final double MAX_SPEED  = Units.feetToMeters(14.5);// Maximum speed of the robot in meters per second, used to limit acceleration.
+  public static final double MAX_SPEED  = Units.feetToMeters(19.5);// Maximum speed of the robot in meters per second, used to limit acceleration.
 
   public static final class DrivebaseConstants
   {
@@ -80,13 +80,13 @@ public final class Constants
     public static final double kRollerI = 0;
     public static final double kRollerD = 0;
 
-    public static final double INTAKE_ROLLER_VELOCITY_RPM = 1200.0;
+    public static final double INTAKE_ROLLER_VELOCITY_RPM = 1000.0;
 
     public static final double kArmV = 0.91;
     public static final double kArmA = 0.042;
     public static final double kArmG = 0.60;
 
-    public static final double kArmP = 0.065; //was 0.05 - had to up this value in an attempt to increase retracting speed.
+    public static final double kArmP = 0.05; //was 0.05 - had to up this value in an attempt to increase retracting speed.
     public static final double kArmI = 0;
     public static final double kArmD = 0;
 
