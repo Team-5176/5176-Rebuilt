fdf5a341dece9daca62575360c029bd37e2b0a4f
SLUH Robotics <robotics@sluh.org>
2026-09-12 10:06:30 -0500

    Some shooter changes

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -47,7 +47,7 @@ public final class Constants
   public static class ShooterConstants // Keep in mind these values affect 2 motors working together
   {
     // Target Shooter Velocity in RPM - May be removed later if using a function to calculate distance then velocity of the ball
-    public static double SHOOTER_TARGET_VELOCITY_RPM = 1050.0; //1800.0;//3000
+    public static double SHOOTER_TARGET_VELOCITY_RPM = 890.0; //1800.0;//3000
 
     // Time to let the shooter wheel spin up to speed before feeding balls into it, for consistent shots
     public static final double SHOOTER_SPINUP_DELAY_SECONDS = 0.75;
